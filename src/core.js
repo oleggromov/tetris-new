4aa2ef7a8da319f71703e183f3786d411ddc2877
Gromych <gromych@yandex-team.ru>
2016-03-12 23:26:48 +0300

    Adds FigureFactory and Timer and starts logging each tick

--- src/core.js
+++ src/core.js
@@ -1,15 +1,23 @@
-var pubsub = require('true-pubsub');
+var FigureFactory = require('./figure-factory.js');
 
 function core (win, doc) {
+    var Timer = require('./timer')(win);
     var Keyboard = require('./keyboard')(doc);
 
     function Game () {
         this._keyboard = new Keyboard();
         this._keyboard.on('press', this.processKey.bind(this));
+
+        this._timer = new Timer(1000);
+        this._timer.on('tick', this.showTick.bind(this));
     }
 
     Game.prototype.processKey = function (type) {
         win.console.log(type);
+    };
+
+    Game.prototype.showTick = function (delta) {
+        win.console.log(delta);
     };
 
     return Game;
