c4290705f307ae576bf24ed85d549cb7b2f3352a
Gromych <gromych@yandex-team.ru>
2016-03-13 12:04:40 +0300

    Adds figure types and removes type selection out from consturctor

--- src/figure.js
+++ src/figure.js
@@ -1,13 +1,47 @@
-function Figure (type) {
-    if (!type) {
-        type = Math.round(Math.random() * this.TYPES.length - 1);
-    }
-
+function Figure (type, color) {
     this._type = type;
+    this._color = color;
+    this._config = this.CONFIG[this.TYPES[type]];
 }
 
 Figure.prototype.TYPES = [
-    'LINE', 'BOX', 'TRIANGLE', 'HOOK_1', 'HOOK_2'
+    'LINE', 'BOX', 'TRIANGLE', 'HOOK_LEFT', 'HOOK_RIGHT'
+];
+
+Figure.prototype.CONFIG = {
+    LINE: [
+        [1],
+        [1],
+        [1],
+        [1]
+    ],
+
+    BOX: [
+        [1, 1],
+        [1, 1]
+    ],
+
+    TRIANGLE: [
+        [1, 0],
+        [1, 1],
+        [1, 0]
+    ],
+
+    HOOK_LEFT: [
+        [0, 1],
+        [1, 1],
+        [1, 0]
+    ],
+
+    HOOK_RIGHT: [
+        [1, 0],
+        [1, 1],
+        [0, 1]
+    ]
+};
+
+Figure.prototype.COLORS = [
+    '#f00', '#0f0', '#00f'
 ];
 
 module.exports = Figure;
