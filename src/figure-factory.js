1ce93390f809fe9ff57c0f9217e0cebfc628803b
Gromych <gromych@yandex-team.ru>
2016-03-13 12:04:57 +0300

    Updates FigureFactory to have getRandom method

--- src/figure-factory.js
+++ src/figure-factory.js
@@ -1,17 +1,24 @@
 var Figure = require('./figure');
 
 function Factory () {
-    this._next = new Figure;
+    this._next = this._getRandom();
 }
 
 Factory.prototype.getCurrent = function () {
     var current = this._next;
-    this._next = new Figure;
+    this._next = this._getRandom();
     return current;
 };
 
 Factory.prototype.getNext = function () {
     return this._next;
+};
+
+Factory.prototype._getRandom = function () {
+    var type = Math.floor(Math.random() * Figure.prototype.TYPES.length);
+    var color = Math.floor(Math.random() * Figure.prototype.COLORS.length);
+
+    return new Figure(type, color);
 };
 
 module.exports = Factory;
