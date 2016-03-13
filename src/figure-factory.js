773349f6c0a42e4cfc18a3cdabd65c7e4f56574d
Gromych <gromych@yandex-team.ru>
2016-03-13 13:57:54 +0300

    Renames FigureFactory methods for them to be more clear

--- src/figure-factory.js
+++ src/figure-factory.js
@@ -1,17 +1,17 @@
 var Figure = require('./figure');
 
 function Factory () {
-    this._next = this._getRandom();
+    this._upcoming = this._getRandom();
 }
 
-Factory.prototype.getCurrent = function () {
-    var current = this._next;
-    this._next = this._getRandom();
-    return current;
+Factory.prototype.getNext = function () {
+    var next = this._upcoming;
+    this._upcoming = this._getRandom();
+    return next;
 };
 
-Factory.prototype.getNext = function () {
-    return this._next;
+Factory.prototype.getUpcoming = function () {
+    return this._upcoming;
 };
 
 Factory.prototype._getRandom = function () {
